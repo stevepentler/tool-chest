29c593bde9d36738c77f631840b8a36a93b28f45
Steve Pentler <stevepentler@gmail.com>
2016-01-06 14:55:09 -0700

    adding user_id to admin_tool_edit form

--- app/controllers/admin/tools_controller.rb
+++ app/controllers/admin/tools_controller.rb
@@ -1,4 +1,33 @@
 class Admin::ToolsController < Admin::BaseController
+  before_action :set_tool, only: [:show]
+  # before_action :set_user, only: [:show]
+  
   def index
+    @tools = Tool.all
+    # @user = @tool.user_id
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_tool 
+    @tool = Tool.find(params[:id])
+  end 
+
+  def set_user
+    # @user = User.find(params[:user_id])
+  end 
+
+  def tool_params
+    params.require(:tool).permit(:name, :use)
+  end
+
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
     resources :tools
   end
 
-
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
