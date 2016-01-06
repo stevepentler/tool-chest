609f5ee7f470e0c3267e238aacfa0983010c5e34
Steve Pentler <stevepentler@gmail.com>
2016-01-05 18:50:40 -0700

    added link to user_tools from user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.clear
-    redirect_to tools_path
+    redirect_to login_path
     flash[:notice] = "Logged Out!"
   end 
 end 
